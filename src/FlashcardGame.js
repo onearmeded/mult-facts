51ba02fdf2750aca4039696ffa730bfc28aedf06
Ed <OneArmedEd@gmail.com>
2020-06-02 22:33:43 -0400

    Add new game button

--- src/FlashcardGame.js
+++ src/FlashcardGame.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Flashcard from './Flashcard';
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import Button from 'react-bootstrap/Button';
 
 export default function FlashcardGame(props)
 {
@@ -17,10 +18,23 @@ export default function FlashcardGame(props)
 
     const currentPercent = () => (score / props.questions) * 100;
 
-    return (
+    const isWinner = () => score >= props.questions;
+
+    const newGame = _ => setScore(0);
+
+    const winnerLayout = (
+        <div>
+            <div>You win!</div>
+            <Button onClick={newGame}>New Game</Button>
+        </div>);
+
+    const gameLayout = (
         <div>
             <Flashcard max={props.max} maxRange = {props.maxRange} 
                 onCorrectAnswer={onCorrectAnswer} onIncorrectAnswer={onIncorrectAnswer} />
             <div style={{margin:'5px'}}><ProgressBar now={currentPercent()} label={score} style={{width:'10em'}} /></div>
-        </div>);
+        </div>
+    );
+
+    return isWinner() ? winnerLayout : gameLayout;
 }
