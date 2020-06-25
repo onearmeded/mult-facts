22cf0350f958a8fd37e8e42f26f4c66b75e4ac89
Ed <OneArmedEd@gmail.com>
2020-06-24 23:07:53 -0400

    Add build folder

---
+++ build/precache-manifest.c1fe8c7019c0f5227182a33c1f9d2be7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee33c171af69023d91fcc1707f5bdbc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ffc22f5fcb5945c8b36",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d168190328c286ac6127",
+    "url": "/static/js/2.9ee03fc1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.9ee03fc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ffc22f5fcb5945c8b36",
+    "url": "/static/js/main.7f009d9a.chunk.js"
+  },
+  {
+    "revision": "8b8c418ddc13be880c13",
+    "url": "/static/js/runtime-main.e3b33d76.js"
+  },
+  {
+    "revision": "9dbaa26362c2c5533a245199ff111b37",
+    "url": "/static/media/lemon-meringue.9dbaa263.png"
+  }
+]);
