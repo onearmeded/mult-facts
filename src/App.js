1161b3a46a5d6f4851737664087c4f4d791180a8
Eddie Deyo <onearmeded@gmail.com>
2021-01-30 17:29:48 -0500

    Change min from 0 to 1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import FlashcardGame from './FlashcardGame';
 
 function App() {
   return (
-    <FlashcardGame min={0} max={15} maxRange={10} maxMultRange={12} possibleMultValues={[0,1,2,3,5,10]} questions={25} />
+    <FlashcardGame min={1} max={15} maxRange={10} maxMultRange={12} possibleMultValues={[0,1,2,3,5,10]} questions={25} />
   );
 }
 
