935964295155e33155d6311ebdef07e70691f0a9
onearmeded <67402544+onearmeded@users.noreply.github.com>
2021-02-01 15:23:21 -0500

    Remove 0 and 1 from multiplication factors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import FlashcardGame from './FlashcardGame';
 
 function App() {
   return (
-    <FlashcardGame min={1} max={15} maxRange={10} maxMultRange={12} possibleMultValues={[0,1,2,3,5,9,10]} questions={25} />
+    <FlashcardGame min={1} max={15} maxRange={10} maxMultRange={12} possibleMultValues={[2,3,5,9,10]} questions={25} />
   );
 }
 
